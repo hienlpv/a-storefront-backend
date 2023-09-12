1e6d144a0011f77384ef08e77b5f0b1fa9f6ef62
hienlpv <hienlpv@gmail.com>
2023-09-12 19:21:58 +0700

    apply cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import 'dotenv/config';
 
 import express from 'express';
+import cors from 'cors';
 
 import config from './config';
 import database from './database';
 
-const { port } = config.server;
-
+// variable
 const app = express();
+const port = config.server.port;
 
+// middleware
+app.use(cors());
+
+// connect DB
 database.connect();
 
+// listen server
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
